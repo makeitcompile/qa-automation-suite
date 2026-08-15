1634c43d5bb82f315151e53a9a120b015ef5f5b0
aql <sreekanth.harigovindan@outlook.com>
2026-08-15 09:48:41 +0300

    feat: handle cookie

--- src/test/java/pages/BankPage.java
+++ src/test/java/pages/BankPage.java
@@ -1,11 +1,23 @@
 package pages;
 
+import com.codeborne.selenide.SelenideElement;
 import utils.ConfigReader;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class BankPage {
 
     public void openPage() {
         open(ConfigReader.get("ui.url"));
+    }
+
+    private static final SelenideElement acceptAllButton =
+            $$("button").findBy(text("ACCEPT ALL"));
+
+    public void acceptCookies() {
+        acceptAllButton.shouldBe(visible).click();
     }
 }
